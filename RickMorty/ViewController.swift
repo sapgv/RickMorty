5b3b6d2433a41681637db3032fa5d46ceb5ac89c
Grigory Sapogov <grisha.sapgv@mail.ru>
2023-11-25 20:21:07 +0500

    Initial Commit

---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Grigory Sapogov on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
